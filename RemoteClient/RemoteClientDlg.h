e67ff181188bf9f738a3297e4d64c06dc3fd120c
wangxinbinTesla <1285173093@qq.com>
2026-05-26 17:26:04 +0800

    新增：客户端获取服务端文件目录信息和展示功能； 调试并验证了功能的有效性

--- RemoteClient/RemoteClientDlg.h
+++ RemoteClient/RemoteClientDlg.h
@@ -22,7 +22,12 @@ public:
 	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
 
 private:
-	int SendCommandPacket(int nCmd, BYTE* pData = NULL, size_t nLength = 0);
+	CString CRemoteClientDlg::GetPath(HTREEITEM hTree);
+	void DeleteTreeChildrenItem(HTREEITEM hTree);
+
+	//1 查看磁盘分区 2 查看指定目录下文件 3 打开文件 4 下载文件
+	//返回值是命令号，如果小于0则错误
+	int SendCommandPacket(int nCmd, bool bAutoClose = true, BYTE* pData = NULL, size_t nLength = 0);
 
 // 实现
 protected:
@@ -40,4 +45,5 @@ public:
 	CString m_nPort;
 	afx_msg void OnBnClickedBtnFileinfo();
 	CTreeCtrl m_Tree;
+	afx_msg void OnNMDblclkTreeDir(NMHDR* pNMHDR, LRESULT* pResult);
 };
