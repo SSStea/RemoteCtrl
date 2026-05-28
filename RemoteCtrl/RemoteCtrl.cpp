7b74521bd348d4e02df35f259b11ad1dbfb0c3c2
wangxinbinTesla <1285173093@qq.com>
2026-05-28 11:52:52 +0800

    Revert "修改：解决了文件目录、文件传输时显示缺失的bug"
    This reverts commit cb9517d41a202d480c20d58eaa635730ab85dd3d.

--- RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl.cpp
@@ -93,8 +93,6 @@ int MakeDirecoryInfo()
         OutputDebugString(_T("未找到任何文件！"));
         return -3;
     }
-
-    int nCount = 0;
     do {
         FILEINFO fInfo;
         fInfo.bIsDirectory = ((fData.attrib & _A_SUBDIR) != 0);
@@ -104,9 +102,7 @@ int MakeDirecoryInfo()
 
         CPacket pack(2, (BYTE*)&fInfo, sizeof(fInfo));
         CServSocket::getInstance()->bSend(pack);//获取一个文件就发送一个
-        nCount++;
     } while (!_findnext(hFind, &fData));
-    TRACE("Server Count = %d\r\n", nCount);
     
     FILEINFO fInfo;
     fInfo.bHasNext = FALSE;//告诉控制端没有下一个文件了，不必继续等待
