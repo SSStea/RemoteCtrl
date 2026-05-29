a2208efd27277639524cb3507208ffeef0ec43e5
wangxinbinTesla <1285173093@qq.com>
2026-05-29 15:21:35 +0800

    新增：设计鼠标远程控制结构；实现本地鼠标信息获取、封装、发送

--- RemoteCtrl/RemoteClient/WatchDialog.cpp
+++ RemoteCtrl/RemoteClient/WatchDialog.cpp
@@ -31,10 +31,19 @@ void CWatchDialog::DoDataExchange(CDataExchange* pDX)
 
 BEGIN_MESSAGE_MAP(CWatchDialog, CDialog)
 	ON_WM_TIMER()
+	ON_WM_LBUTTONDBLCLK()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_WM_RBUTTONDBLCLK()
+	ON_WM_RBUTTONDOWN()
+	ON_WM_RBUTTONUP()
+	ON_WM_MOUSEMOVE()
+	ON_STN_CLICKED(IDC_WATCH, &CWatchDialog::OnStnClickedWatch)
 END_MESSAGE_MAP()
 
 
 // CWatchDialog 消息处理程序
+
 
 BOOL CWatchDialog::OnInitDialog()
 {
@@ -74,4 +83,146 @@ void CWatchDialog::OnTimer(UINT_PTR nIDEvent)
 		}
 	}
 	CDialog::OnTimer(nIDEvent);
+}
+
+CPoint CWatchDialog::UserPoint2RemoteScreenPoint(CPoint& point)
+{//client:800*450
+	CRect clientRect;
+	ScreenToClient(&point);//将屏幕坐标point转化为客户端坐标
+	m_picture.GetWindowRect(&clientRect);//获取客户端的Rect：800*450
+	int width0 = clientRect.Width();
+	int height0 = clientRect.Height();
+	int width = 1920, height = 1080;//远程机的Rect
+	int x = point.x * width / width0;
+	int y = point.y * height / height0;//远程机的坐标
+
+	return CPoint(x, y);
+}
+
+void CWatchDialog::OnLButtonDblClk(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CPoint remote = UserPoint2RemoteScreenPoint(point);
+
+	MOUSEEVENT event;
+	event.ptXY = remote;
+	event.nButton = 0;
+	event.nAction = 2;//双击
+
+	CClientSocket* pClient = CClientSocket::getInstance();
+	CPacket pack(5, (BYTE*)&event, sizeof(event));
+	pClient->bSend(pack);
+
+	CDialog::OnLButtonDblClk(nFlags, point);
+}
+
+void CWatchDialog::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	
+
+	CDialog::OnLButtonDown(nFlags, point);
+}
+
+void CWatchDialog::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	//CPoint remote = UserPoint2RemoteScreenPoint(point);
+
+	//MOUSEEVENT event;
+	//event.ptXY = remote;
+	//event.nButton = 0;
+	//event.nAction = 4;//弹起
+
+	//CClientSocket* pClient = CClientSocket::getInstance();
+	//CPacket pack(5, (BYTE*)&event, sizeof(event));
+	//pClient->bSend(pack);
+
+	CDialog::OnLButtonUp(nFlags, point);
+}
+
+void CWatchDialog::OnRButtonDblClk(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CPoint remote = UserPoint2RemoteScreenPoint(point);
+
+	MOUSEEVENT event;
+	event.ptXY = remote;
+	event.nButton = 2;
+	event.nAction = 2;//双击
+
+	CClientSocket* pClient = CClientSocket::getInstance();
+	CPacket pack(5, (BYTE*)&event, sizeof(event));
+	pClient->bSend(pack);
+
+	CDialog::OnRButtonDblClk(nFlags, point);
+}
+
+void CWatchDialog::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CPoint remote = UserPoint2RemoteScreenPoint(point);
+
+	MOUSEEVENT event;
+	event.ptXY = remote;
+	event.nButton = 2;
+	event.nAction = 3;//按下， TODO：服务端要做对应修改
+
+	CClientSocket* pClient = CClientSocket::getInstance();
+	CPacket pack(5, (BYTE*)&event, sizeof(event));
+	pClient->bSend(pack);
+
+	CDialog::OnRButtonDown(nFlags, point);
+}
+
+void CWatchDialog::OnRButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CPoint remote = UserPoint2RemoteScreenPoint(point);
+
+	MOUSEEVENT event;
+	event.ptXY = remote;
+	event.nButton = 2;
+	event.nAction = 4;//弹起
+
+	CClientSocket* pClient = CClientSocket::getInstance();
+	CPacket pack(5, (BYTE*)&event, sizeof(event));
+	pClient->bSend(pack);
+
+	CDialog::OnRButtonUp(nFlags, point);
+}
+
+void CWatchDialog::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CPoint remote = UserPoint2RemoteScreenPoint(point);
+
+	MOUSEEVENT event;
+	event.ptXY = remote;
+	event.nButton = 0;
+	event.nAction = 1;//移动
+
+	CClientSocket* pClient = CClientSocket::getInstance();
+	CPacket pack(5, (BYTE*)&event, sizeof(event));
+	pClient->bSend(pack);
+
+	CDialog::OnMouseMove(nFlags, point);
+}
+
+void CWatchDialog::OnStnClickedWatch()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CPoint point;
+	GetCursorPos(&point);
+
+	CPoint remote = UserPoint2RemoteScreenPoint(point);
+
+	MOUSEEVENT event;
+	event.ptXY = remote;
+	event.nButton = 0;
+	event.nAction = 3;//按下
+
+	CClientSocket* pClient = CClientSocket::getInstance();
+	CPacket pack(5, (BYTE*)&event, sizeof(event));
+	pClient->bSend(pack);
 }
