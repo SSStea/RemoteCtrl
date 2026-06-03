fbcf03f4d96cd496773f16b1bbf3cb05fd3a9fe1
wangxinbinTesla <1285173093@qq.com>
2026-06-03 14:34:57 +0800

    新增：命令类、工具类，将原服务端RemoteCtrl.cpp中的工具函数和命令处理函数封装成类的成员函数，不再暴露为全局函数

---
+++ RemoteCtrl/Command.cpp
@@ -0,0 +1,41 @@
+#include "pch.h"
+#include "Command.h"
+
+CCommand::CCommand():threadid(0)
+{
+	struct 
+	{
+		int nCmd;
+		CMDFUNC func;
+	}data[] = {
+		{1, &CCommand::MakeDriverInfo},
+		{2, &CCommand::MakeDirecoryInfo},
+		{3, &CCommand::RunFile},
+		{4, &CCommand::DownLoadFile},
+		{5, &CCommand::MouseEvent},
+		{6, &CCommand::SendScreen},
+		{7, &CCommand::LockMachine},
+		{8, &CCommand::UnLockMachine},
+		{9, &CCommand::DeleteLocalFile},
+		{1981, &CCommand::TestConnect},
+		{-1, NULL}
+	};//一个数组结构体
+
+	for (int i = 0; data[i].nCmd != -1; i++)
+	{
+		//将数组结构体中的命令和对应命令处理函数的映射存到哈希表中
+		m_mapFuntion.insert(std::pair<int, CMDFUNC>(data[i].nCmd, data[i].func));
+	}
+}
+
+int CCommand::ExcuteCmd(int nCmd)
+{
+	auto it = m_mapFuntion.find(nCmd);
+	if (it == m_mapFuntion.end())
+	{
+		return -1;
+	}
+
+	return (this->*it->second)();
+	//迭代器it的second是类的成员函数指针
+}
