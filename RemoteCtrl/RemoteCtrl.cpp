5a4958a7c55a78f79e15ce1679ebb317bbc4be94
wangxinbinTesla <1285173093@qq.com>
2026-06-12 16:17:51 +0800

    新增：追加启动权限请求，开机自启动功能

--- RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl.cpp
@@ -18,6 +18,56 @@ CWinApp theApp;
 
 using namespace std;
 
+void ChooseAutoInvoke()
+{
+    CString strSubKey = _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+    CString strInfo = _T("该程序只允许用于合法的用途！\n");
+    strInfo += _T("继续运行该程序，将使得这台机器处于被监控状态\n");
+    strInfo += _T("如果你不希望这样，请点击“取消”按钮，退出程序\n");
+    strInfo += _T("按下“是”按钮，该程序将被复制到你的机器上，并随系统启动而自动运行\n");
+    strInfo += _T("按下“否”按钮，程序只运行一次，不会在系统内留下任何东西\n");
+
+    int nRet = MessageBox(NULL, strInfo, _T("WARNING"), MB_YESNOCANCEL | MB_ICONWARNING | MB_TOPMOST);
+    if (nRet == IDYES)
+    {
+        char sPath[MAX_PATH] = "";
+        char sSys[MAX_PATH] = "";
+        std::string strExe = "\\RemoteCtrl.exe ";
+        GetCurrentDirectoryA(MAX_PATH, sPath);
+        GetSystemDirectoryA(sSys, sizeof(sSys));
+        std::string strCmd = "mklink " + std::string(sSys) + strExe + std::string(sPath) + strExe;
+       
+        system(strCmd.c_str());
+
+        HKEY hKey = NULL;
+        nRet = RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubKey, 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &hKey);
+        if (nRet != ERROR_SUCCESS)
+        {
+            RegCloseKey(hKey);
+            MessageBox(NULL, _T("设置自动开机启动失败！\r\n是否权限不足？"),
+                _T("ERROR"), MB_ICONERROR | MB_TOPMOST);
+            exit(0);
+        }
+
+        CString strPath = _T("%SystemRoot%\\system32\\RemoteCtrl.exe");
+        nRet = RegSetValueEx(hKey, _T("RemoteCtrl"), 0, REG_EXPAND_SZ,
+            (BYTE*)(LPCTSTR)strPath, strPath.GetLength()*sizeof(TCHAR));
+        if (nRet != ERROR_SUCCESS)
+        {
+			RegCloseKey(hKey);
+			MessageBox(NULL, _T("设置自动开机启动失败！\r\n是否权限不足？"),
+				_T("ERROR"), MB_ICONERROR | MB_TOPMOST);
+			exit(0);
+        }
+
+        RegCloseKey(hKey);
+    }
+    else if (nRet == IDCANCEL)
+    {
+        exit(0);
+    }
+    return;
+}
 
 int main()
 {
@@ -40,6 +90,7 @@ int main()
         {
             // 创建命令处理模块
             CCommand cmd;
+            ChooseAutoInvoke();
 
             // 取得服务端 socket 单例。
             // getInstance 会确保 Winsock 环境已经初始化，并且服务端 socket 对象只创建一次。
