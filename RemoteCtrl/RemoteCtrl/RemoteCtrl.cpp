d9a2eb77cad6152157dc28c87a71754d502507dc
wangxinbinTesla <1285173093@qq.com>
2026-06-13 14:33:38 +0800

    新增：udp穿透打洞代码框架

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -11,6 +11,8 @@
 #define new DEBUG_NEW
 #endif
 
+//系统变量路径
+#define SYSTEMPATH TEXT("C:\\Windows\\System32\\RemoteCtrl.exe")
 
 // 唯一的应用程序对象
 
@@ -22,13 +24,8 @@ using namespace std;
 //如果两者权限不一致，则会导致程序启动失败
 //开机启动对环境变量有影响，如果依赖dll库则可能启动失败
 //【可以通过将dll库复制到system32或sysWOW64下面】
-void ChooseAutoInvoke()
+bool ChooseAutoInvoke()
 {
-    CString strPath = _T("C:\\Windows\\System32\\RemoteCtrl.exe");
-    if (PathFileExists(strPath))
-    {
-        return;
-    }
     CString strSubKey = _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
     CString strInfo = _T("该程序只允许用于合法的用途！\n");
     strInfo += _T("继续运行该程序，将使得这台机器处于被监控状态\n");
@@ -37,103 +34,125 @@ void ChooseAutoInvoke()
     strInfo += _T("按下“否”按钮，程序只运行一次，不会在系统内留下任何东西\n");
 
     int nRet = MessageBox(NULL, strInfo, _T("WARNING"), MB_YESNOCANCEL | MB_ICONWARNING | MB_TOPMOST);
-    if (nRet == IDYES)
-    {
-        char sPath[MAX_PATH] = "";
-        char sSys[MAX_PATH] = "";
-        std::string strExe = "\\RemoteCtrl.exe ";
-        GetCurrentDirectoryA(MAX_PATH, sPath);
-        GetSystemDirectoryA(sSys, sizeof(sSys));
-        std::string strCmd = "mklink " + std::string(sSys) + strExe + std::string(sPath) + strExe;
-       
-        system(strCmd.c_str());
-
-        HKEY hKey = NULL;
-        nRet = RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubKey, 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &hKey);
-        if (nRet != ERROR_SUCCESS)
-        {
-            RegCloseKey(hKey);
-            MessageBox(NULL, _T("设置自动开机启动失败！\r\n是否权限不足？"),
-                _T("ERROR"), MB_ICONERROR | MB_TOPMOST);
-            exit(0);
-        }
-
-        nRet = RegSetValueEx(hKey, _T("RemoteCtrl"), 0, REG_EXPAND_SZ,
-            (BYTE*)(LPCTSTR)strPath, strPath.GetLength()*sizeof(TCHAR));
-        if (nRet != ERROR_SUCCESS)
-        {
-			RegCloseKey(hKey);
-			MessageBox(NULL, _T("设置自动开机启动失败！\r\n是否权限不足？"),
-				_T("ERROR"), MB_ICONERROR | MB_TOPMOST);
-			exit(0);
-        }
-
-        RegCloseKey(hKey);
-    }
-    else if (nRet == IDCANCEL)
-    {
-        exit(0);
-    }
-    return;
+	if (nRet == IDYES)
+	{
+		if (!CEdoyunTool::WriteRefisterTable(SYSTEMPATH)) {}//尝试方式二设置开机自启 写入注册表自启动 
+		else
+		{
+			MessageBox(NULL, TEXT("设置开机自启动失败\r\n"), TEXT("错误"), MB_ICONERROR | MB_TOPMOST);
+			return false;
+		}
+	}
+	else if (nRet == IDCANCEL) return false;
+	return true;
 }
 
-int main()
+void udp_server();
+
+void udp_client(bool bIsHost = true);
+
+int main(int argc, char* argv[])
 {
-    int nRetCode = 0;
+	//if (!CEdoyunTool::IsAdmin())//管理员用户  TODO:这里条件取反 为了测试方便避免提权操作(也就是普通用户会进入此逻辑)
+	//{
+	if (!CEdoyunTool::Init()) return 1;//MFC命令行项目初始化
+	//MessageBox(NULL, TEXT("管理员"), TEXT("用户状态"), 0);
+	//if (ChooseAutoInvoke())//设置开机自启
+	//{
+ //       // 创建命令处理模块
+ //       CCommand cmd;
+ //       ChooseAutoInvoke();
 
-    // 获取当前程序模块句柄，MFC 初始化需要用到它。
-    HMODULE hModule = ::GetModuleHandle(nullptr);
+ //       // 取得服务端 socket 单例。
+ //       // getInstance 会确保 Winsock 环境已经初始化，并且服务端 socket 对象只创建一次。
+ //       CServSocket*    pServer = CServSocket::getInstance();
 
-    if (hModule != nullptr)
-    {
-        // 初始化 MFC 并在失败时显示错误。
-        // 这里虽然是控制台入口 main，但项目仍然使用了 MFC 的部分能力，例如 MessageBox。
-        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
-        {
-            // TODO: 在此处为应用程序的行为编写代码。
-            wprintf(L"错误: MFC 初始化失败\n");
-            nRetCode = 1;
-        }
-        else
-        {
-            // 创建命令处理模块
-            CCommand cmd;
-            ChooseAutoInvoke();
+ //       // 网络单例socket的一系列操作、处理接收到的命令
+ //       int nRet = pServer->nRun(&CCommand::RunCommand, &cmd);
+ //       switch (nRet)
+ //       {
+ //       case -1:
+	//		{
+	//			MessageBox(NULL, _T("网络初始化异常，未能成功初始化，请检查网络状态"),
+	//				_T("网络初始化失败"), MB_OK | MB_ICONERROR);
+	//			exit(0);
+	//		}
+ //           break;
+ //       case -2:
+ //           {
+	//			MessageBox(NULL, _T("多次无法正常接入用户，结束程序"),
+	//				_T("接入用户失败！"), MB_OK | MB_ICONERROR);
+	//			exit(0);
+ //           }
+ //           break;
+ //       default:
+ //           break;
+ //       }     
+	//}
 
-            // 取得服务端 socket 单例。
-            // getInstance 会确保 Winsock 环境已经初始化，并且服务端 socket 对象只创建一次。
-            CServSocket*    pServer = CServSocket::getInstance();
+	if (argc == 1)
+	{
+		char wstrDir[MAX_PATH];
+		GetCurrentDirectoryA(MAX_PATH, wstrDir);
+		STARTUPINFOA si;
+		PROCESS_INFORMATION pi;
+		memset(&si, 0, sizeof(si));
+		memset(&pi, 0, sizeof(pi));
+		std::string strCmd = argv[0];
+		strCmd += " 1";
+		BOOL bRet = CreateProcessA(NULL, (LPSTR)strCmd.c_str(), NULL, NULL, FALSE, 0, NULL, wstrDir, &si, &pi);
+		if (bRet)
+		{
+			CloseHandle(pi.hThread);
+			CloseHandle(pi.hProcess);
+			TRACE("进程ID：%d\r\n", pi.dwProcessId);
+			TRACE("线程ID：%d\r\n", pi.dwThreadId);
 
-            // 网络单例socket的一系列操作、处理接收到的命令
-            int nRet = pServer->nRun(&CCommand::RunCommand, &cmd);
-            switch (nRet)
-            {
-            case -1:
-			    {
-				    MessageBox(NULL, _T("网络初始化异常，未能成功初始化，请检查网络状态"),
-					    _T("网络初始化失败"), MB_OK | MB_ICONERROR);
-				    exit(0);
-			    }
-                break;
-            case -2:
-                {
-				    MessageBox(NULL, _T("多次无法正常接入用户，结束程序"),
-					    _T("接入用户失败！"), MB_OK | MB_ICONERROR);
-				    exit(0);
-                }
-                break;
-            default:
-                break;
-            }     
-        }
-    }
-    else
-    {
-        // TODO: 更改错误代码以符合需要
-        wprintf(L"错误: GetModuleHandle 失败\n");
-        nRetCode = 1;
-    }
+			strCmd += " 2";
+			bRet = CreateProcessA(NULL, (LPSTR)strCmd.c_str(), NULL, NULL, FALSE, 0, NULL, wstrDir, &si, &pi);
+			if (bRet)
+			{
+				CloseHandle(pi.hThread);
+				CloseHandle(pi.hProcess);
+				TRACE("进程ID：%d\r\n", pi.dwProcessId);
+				TRACE("线程ID：%d\r\n", pi.dwThreadId);
 
-    return nRetCode;
+				udp_server();//服务器
+			}
+		}
+	}
+	else if (argc == 2)
+	{//主客户端
+		udp_client();
+	}
+	else
+	{
+		udp_client(false);
+	}
+	//}
+	//else//普通用户   需要提权创建新进程
+	//{
+	//	if (CEdoyunTool::RunAsAdmin() == false) return 1;
+	//	MessageBox(NULL, TEXT("普通用户"), TEXT("用户状态"), 0);
+	//}
+	return 0;
 }
 
+void udp_server()
+{
+	printf("%s(%d):%s\r\n", __FILE__, __LINE__, __FUNCTION__);
+
+	getchar();
+}
+
+void udp_client(bool bIsHost)
+{
+	if (bIsHost)
+	{
+		printf("%s(%d):%s\r\n", __FILE__, __LINE__, __FUNCTION__);
+	}
+	else
+	{
+		printf("%s(%d):%s\r\n", __FILE__, __LINE__, __FUNCTION__);
+	}
+}
